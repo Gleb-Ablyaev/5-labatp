62dee3d37065a482a530753fd0ad32e90f3f62cc
Gleb-Ablyaev <99827453+Gleb-Ablyaev@users.noreply.github.com>
2023-06-12 02:35:23 +0400

    Add files via upload

---
+++ Models/model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class model
+    {
+        [DisplayName("id")]
+        public string id { get; set; }
+
+        [DisplayName("баланс игрока")]
+        public string balance { get; set; }
+
+        [DisplayName("Имя игрока")]
+        public string igrokname { get; set; }
+
+        [DisplayName("Название букмекерской конторы")]
+        public string bk { get; set; }
+
+        [DisplayName("Дата")]
+        [DataType(DataType.Date)]
+        public DateTime date { get; set; }
+
+        [DisplayName("Номер телефона")]
+        public string PhoneNumber { get; set; }
+    }
+
+}
